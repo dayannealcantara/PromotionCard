95db3ad738db0af100cd0a68015b3af860800425
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-03-01 10:54:52 -0300

    feat: uso de Switch react-router-dom

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { 
-  BrowserRouter as Router , Route } from "react-router-dom";
+  BrowserRouter as Router , Route, Switch } from "react-router-dom";
 import PagesPromotionSearch from "./Promotion/Search";
 import PagesPromotionForm from "./Promotion/Form";
 
 
 const Root = () => {
   return (
-    <Router>     
+    <Router> 
+      <Switch> 
       <Route path="/create" component={PagesPromotionForm}/>
       <Route path="/edit/:id" component={PagesPromotionForm}/>
-      <Route path="/" component={PagesPromotionSearch}/>    
+      <Route path="/" component={PagesPromotionSearch}/>  
+      </Switch>     
     </Router>
   )
 }
