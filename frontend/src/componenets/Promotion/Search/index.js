3ed804543ac6f67539c47b38b269a8f9d8e3e220
Dayanne Alcantara <dayannealcantara14@gmail.com>
2022-05-13 14:24:58 -0300

    feat: ajustes

--- frontend/src/componenets/Promotion/Search/index.js
+++ frontend/src/componenets/Promotion/Search/index.js
@@ -3,26 +3,34 @@ import { Link } from "react-router-dom";
 import './style.css'
 import PromotionList from "../List";
 import useApi from "componenets/Utils";
+import UIInfiniteScroll from "componenets/UI/InfiniteScroll.js";
+import UIButton from "componenets/UI/Button";
 
+const baseParams = {
+  _embed: 'comments',
+  _order: 'desc',
+  _sort: 'id',
+  _limit: 5,
+}
 
 const PromotionSearch = () =>{
+  const [page, setPage] = useState(1);
   const mountRef = useRef(null)
   const [search, setSearch] = useState('')
   const [load, loadInfo] = useApi({
     debounceDelay: 300,
     url: '/promotions',
     method: 'get',
-    params: {
-      _embed: 'comments',
-      _order: 'desc',
-      _sort: 'id',
-      title_like: search || undefined,
-    },
   });
 
   useEffect(() => { 
    load({
      debounced: mountRef.current,
+     params: {
+       ...baseParams,
+       _page:1,
+       title_like: search || undefined,
+     },
    });
    if (!mountRef.current) {
      mountRef.current = true;
@@ -30,12 +38,33 @@ const PromotionSearch = () =>{
    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [search]);
 
+  function fetchMore() {
+    const newPage = page + 1;
+    load({
+      isFetchMore: true,
+      params: {
+        ...baseParams,
+        _page: newPage,
+        title_like: search || undefined,
+      },
+      updateRequestInfo: (newRequestInfo, prevRequestInfo) => ({
+        ...newRequestInfo,
+        data: [...prevRequestInfo.data, ...newRequestInfo.data],
+      }),
+    });
+    setPage(newPage)
+  }
+
   return (
     <div className="promotion-search">
       <header className="promotion-header">
-        <h1>Promo Show</h1>
-        <Link to="/create">Nova Promoção</Link>
-      </header>     
+        <h1>Prom Show</h1>
+        <UIButton component={Link} to="/create" theme="containedColor">Nova Promoção</UIButton>
+      </header> 
+
+
+      
+          
         <input 
         className="promotion-input" 
         type="search"
@@ -43,8 +72,26 @@ const PromotionSearch = () =>{
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         />
-        <PromotionList promotions={loadInfo.data} loading={loadInfo.loading} error={ loadInfo.error}/>     
-          </div>
+
+
+
+
+        <PromotionList 
+        promotions={loadInfo.data} 
+        loading={loadInfo.loading} 
+        error={ loadInfo.error}
+        refetch={() => {
+          load({
+            params: baseParams,
+          })
+        }}
+        />
+        {loadInfo.data &&
+         !loadInfo.loading &&
+         loadInfo.data?.length < loadInfo.total && (
+           <UIInfiniteScroll fetchMore={fetchMore} />
+        )}
+    </div>
   )
 };
 
